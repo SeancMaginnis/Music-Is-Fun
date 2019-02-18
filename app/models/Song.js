08496f479fa64c026e45a9466ef3af6d56f52187
seancmaginnis <seancmaginnis@gmail.com>
2019-02-17 19:26:03 -0700

    added price

--- app/models/Song.js
+++ app/models/Song.js
@@ -1,21 +1,22 @@
 export default class Song {
-    constructor(song) {
-        this.title = song.trackName
-        //Change 250x250 if you want a larger image
-        this.albumArt = song.artworkUrl60.replace(/60x60/g, "250x250")
-        this.artist = song.artistName
-        this.collection = song.collectionName
-        this.price = song.collectionPrice
-        this.preview = song.previewUrl
-    }
-    getTemplate() {
-        return `
+  constructor(song) {
+    this.title = song.trackName
+    //Change 250x250 if you want a larger image
+    this.albumArt = song.artworkUrl60.replace(/60x60/g, "250x250")
+    this.artist = song.artistName
+    this.collection = song.collectionName
+    this.price = song.collectionPrice
+    this.preview = song.previewUrl
+  }
+  getTemplate() {
+    return `
     <div class="card col-3">
   <img class="card-img-top" src="${this.albumArt}" >
   <div class="card-body">
-    <h5 class="card-title">"${this.title}"</h5>
-    <h6 class="card-text">"${this.artist}"</h6>
-    <p class="card-text">"${this.collection}"</p>
+    <h4 class="card-title">"${this.title}"</h4>
+    <h5 class="card-text">"${this.artist}"</h5>
+    <h6 class="card-text">"${this.collection}"</h6>
+    <h7 class="card-text">"${this.price}"</h7>
     <audio controls class="w-100" src="${this.preview}">
             Your browser does not support the
             <code>audio</code> element.
@@ -23,6 +24,6 @@ export default class Song {
   </div>
 </div>
     </div>`
-    }
+  }
 }
 
